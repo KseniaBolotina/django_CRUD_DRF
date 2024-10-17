56f190d7c948badc495bd9f2490811120c0fc4db
Ksenia Bolotina <reyksenia@yandex.ru>
2024-10-17 19:06:10 +0300

    changed views

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -12,4 +13,5 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['products']
